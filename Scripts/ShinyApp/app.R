fde39ccf0285e980f77dcfdfff2c7b53c79530b5
hillad3 <adam.x.hill@gmail.com>
2024-01-05 23:42:44 -0500

    Stable commit. Unified get_player_stat functions for offense and kicking.

--- Scripts/ShinyApp/app.R
+++ Scripts/ShinyApp/app.R
@@ -30,69 +30,42 @@ get_team_names <- function(season_teams_list = season_teams){
   return(dt)
 }
 
-get_offensive_player_stats <- function(season_year_int = season_year, 
+get_player_stats <- function(stat_type_char, # either 'offense' or 'kicking'
+                                       season_year_int = season_year, 
                                        season_type_char = season_type, 
                                        season_teams_list = season_teams){
   # create data.table for players, which is a combination of the offensive scorers plus kickers
-  dt <- data.table::as.data.table(nflreadr::load_player_stats(seasons = season_year_int, stat_type = 'offense'))
+  dt <- data.table::as.data.table(nflreadr::load_player_stats(seasons = season_year_int, stat_type = stat_type_char))
   dt <- dt[season_type %in% season_type_char]
   
-  setnames(dt, old=c('recent_team'), new=c('team_abbr'))
+  if(stat_type_char == 'offense'){setnames(dt, old=c('recent_team'), new=c('team_abbr'))} 
+  if(stat_type_char == 'kicking'){setnames(dt, old=c('team'), new=c('team_abbr'))}
   dt <- dt[team_abbr %in% season_teams_list]
   
-  # Full Backs are considered running backs for the analysis
-  dt <- dt[position %in% c('QB', 'RB', 'FB', 'WR', 'TE')]
-  dt[,position := if_else(position == 'FB', 'RB', position)]
+  if(stat_type_char=='offense'){
+    dt <- dt[position %in% c('QB', 'RB', 'FB', 'WR', 'TE')]
+    dt[,position := if_else(position == 'FB', 'RB', position)]
+  }
+  if(stat_type_char=='kicking'){dt[,position := 'K']}# position is not in the original dataset
   
-  # consolidate fumbles lost and 2pt conversions into one statistic
-  dt[,fumbles_lost := sack_fumbles_lost + rushing_fumbles_lost + receiving_fumbles_lost]
-  dt[,two_pt_conversions := passing_2pt_conversions + rushing_2pt_conversions + receiving_2pt_conversions]
+  if(stat_type_char=='offense'){
+    # consolidate fumbles lost and 2pt conversions into one statistic
+    dt[,fumbles_lost := sack_fumbles_lost + rushing_fumbles_lost + receiving_fumbles_lost]
+    dt[,two_pt_conversions := passing_2pt_conversions + rushing_2pt_conversions + receiving_2pt_conversions]
+  }
+  if(stat_type_char=='kicking'){dt[,fg_made_50_ := fg_made_50_59 + fg_made_60_]}
   
-  # order columns
-  dt <- dt[
-    , .(
-      position,
-      week,
-      player_id,
-      player_name,
-      team_abbr,
-      passing_yards,
-      passing_tds,
-      rushing_yards,
-      rushing_tds,
-      receiving_yards,
-      receiving_tds,
-      interceptions,
-      sacks,
-      fumbles_lost,
-      two_pt_conversions
-    )
-  ]
-  
-  # change data types to double prior to melting
-  vars <- c(
-    'passing_yards',
-    'passing_tds',
-    'rushing_yards',
-    'rushing_tds',
-    'receiving_yards',
-    'receiving_tds',
-    'interceptions',
-    'sacks',
-    'fumbles_lost',
-    'two_pt_conversions'
+  standard_cols <- c(
+    'position',
+    'week',
+    'player_id',
+    'player_name',
+    'team_abbr',
   )
-  dt[,c(vars) := lapply(.SD, as.numeric), .SDcols=vars]
   
-  # melt into long format
-  dt <- melt(
-    dt,
-    id.vars = c('position',
-                'week',
-                'player_id',
-                'player_name',
-                'team_abbr'),
-    measure.vars = c(
+  
+  if(stat_type_char=='offense'){
+    stat_cols <- c(
       'passing_yards',
       'passing_tds',
       'rushing_yards',
@@ -103,7 +76,30 @@ get_offensive_player_stats <- function(season_year_int = season_year,
       'sacks',
       'fumbles_lost',
       'two_pt_conversions'
-    ),
+    ) 
+  } else if(stat_type_char=='kicking'){
+    stat_cols <- c(
+      'fg_made',
+      'fg_made_40_49',
+      'fg_made_50_',
+      'fg_missed',
+      'fg_missed_list',
+      'fg_blocked',
+      'fg_blocked_list',
+      'pat_made',
+      'pat_missed'
+    ) 
+  }
+  dt <- dt[, .SD, .SDcols = c(standard_cols, stat_cols)] # order cols
+  
+  # change data types to double prior to melting
+  dt[,c(vars) := lapply(.SD, as.numeric), .SDcols=stat_cols]
+  
+  # melt into long format
+  dt <- melt(
+    dt,
+    id.vars = standard_cols,
+    measure.vars = stat_cols,
     variable.factor = FALSE,
     variable.name = 'stat_label',
     value.name = 'football_value'
@@ -121,84 +117,6 @@ get_offensive_player_stats <- function(season_year_int = season_year,
     stat_label %in% c('passing_2pt_conversions', 'rushing_2pt_conversions','receiving_2pt_conversions') ~ as.integer(football_value) * 2L,
     stat_label == 'interceptions' ~ as.integer(football_value) * -2L,
     stat_label %in% c('sack_fumbles_lost', 'rushing_fumbles_lost', 'receiving_fumbles_lost') ~ as.integer(football_value) * -2L,
-    .default = 0L
-  )
-  ]
-  
-}
-
-get_kicker_player_stats <- function(season_year_int = season_year, 
-                                    season_type_char = season_type, 
-                                    season_teams_list = season_teams){
-  
-  dt <- data.table::as.data.table(nflreadr::load_player_stats(seasons = season_year_int, stat_type = 'kicking'))
-  dt <- dt[season_type %in% season_type_char]
-  
-  setnames(dt, old=c('team'), new=c('team_abbr'))
-  dt <- dt[team_abbr %in% season_teams_list]
-  
-  # position is not in the original dataset
-  dt[,position := 'K']
-  
-  # consolidate variables for fantasy scoring
-  dt[,fg_made_50_ := fg_made_50_59 + fg_made_60_]
-  
-  # order columns
-  dt <- dt[
-    , .(
-      position,
-      week,
-      player_id,
-      player_name,
-      team_abbr,
-      fg_made,
-      fg_made_40_49,
-      fg_made_50_,
-      fg_missed,
-      fg_missed_list,
-      fg_blocked,
-      fg_blocked_list,
-      pat_made,
-      pat_missed
-    )
-  ]
-  
-  # change data types to double prior to melting
-  vars <- c(
-    'fg_made',
-    'fg_made_40_49',
-    'fg_made_50_',
-    'fg_missed',
-    'fg_blocked',
-    'pat_made',
-    'pat_missed'
-  )
-  dt[,c(vars) := lapply(.SD, as.numeric), .SDcols=vars]
-  
-  # melt data into long format
-  dt <- melt(
-    dt,
-    id.vars = c('position',
-                'week',
-                'player_id',
-                'player_name',
-                'team_abbr'),
-    measure.vars = c(
-      'fg_made',
-      'fg_made_40_49',
-      'fg_made_50_',
-      'fg_missed',
-      'fg_blocked',
-      'pat_made',
-      'pat_missed'
-    ),
-    variable.factor = FALSE,
-    variable.name = 'stat_label',
-    value.name = 'football_value'
-  )
-  
-  # calculate fantasy points
-  dt[, fantasy_points := case_when(
     stat_label == 'fg_made' ~ as.integer(football_value) * 3L,
     stat_label == 'fg_made_40_49' ~ as.integer(football_value) * 1L, # this is a bonus
     stat_label == 'fg_made_50_' ~ as.integer(football_value) * 2L, # this is a bonus
@@ -206,14 +124,15 @@ get_kicker_player_stats <- function(season_year_int = season_year,
     stat_label == 'pat_made' ~ as.integer(football_value) * 1L,
     stat_label == 'pat_missed' ~ as.integer(football_value) * -1L,
     .default = 0L
-  )]
+  )
+  ]
   
 }
 
 get_player_stats <- function(teams = dt_nfl_teams){
   
   # bind rows
-  dt <- rbindlist(list(get_offensive_player_stats(), get_kicker_player_stats()))
+  dt <- rbindlist(list(get_offensive_player_stats('offense'), get_kicker_player_stats('kicking')))
   
   # join in team conf and division
   dt <- merge(dt, teams[,.(team_abbr, team_conf, team_division)], all.x = TRUE)
