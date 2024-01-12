5aa6b21910c71148ce70f37e0039067f7b5abf19
hillad3 <adam.x.hill@gmail.com>
2024-01-12 03:15:26 -0500

    Cleaned up pipeline for where actions reside in scripts, but no major changes overall.

--- Scripts/Score Playoff Rosters - 2023-2024 Season.R
+++ Scripts/Score Playoff Rosters - 2023-2024 Season.R
@@ -11,72 +11,21 @@ library(nflreadr)
 library(openxlsx)
 
 roster_dir <- "Output/Compiled Rosters/"
-roster_file <- "Playoff Fantasy Roster for 2023-2024, Compiled 2024-01-10 012321.csv"
+roster_file <- "Playoff Fantasy Rosters for 2023-2024, Compiled 2024-01-12 030332.csv"
 
-roster_full <- fread(file = paste0(roster_dir,roster_file))
+rosters <- fread(file = paste0(roster_dir,roster_file))
+rosters[,team_abbr:=NULL]
 
-# drop unnecessary columns because they are in the stats data already
-roster_full[,`Team Abbr.`:=NULL]
-roster_full[,`Position Group`:=NULL]
-roster_full[,`Selection`:=NULL]
-roster_full[,`Check 1 - Selection is Unique`:=NULL]
-roster_full[,`Check 2 - Team is Unique`:=NULL]
-
-setnames(
-  roster_full,
-  old = c(
-    "Fantasy Owner",
-    "Fantasy Team Name",
-    "Automation Mapping",
-    "Roster",
-    "Position Code",
-    "Position Type"
-  ),
-  new = c(
-    "fantasy_owner",
-    "fantasy_team_name",
-    "player_id",
-    "roster",
-    "position_code",
-    "position_type"
-  )
-)
-
-roster_full[,position_code:=ifelse(position_code=="D","Defense",position_code)]
-
-roster_full <- roster_full[, fantasy_owner_initials:=lapply(
-    fantasy_owner,
-    function(x){
-      if(str_detect(x," ")){
-        paste0(str_sub(x,1,1),".",str_sub(x,str_locate(x," ")[,1]+1,str_locate(x," ")[,1]+1),".")
-      } else {
-        paste0(str_sub(x,1,3),".")
-      }
-    }
-  )
-]
-roster_full[,fantasy_owner_initials:=unlist(fantasy_owner_initials)]
-roster_full[,fantasy_owner:=NULL]
+rosters[,position_code:=ifelse(position_code=="D","Defense",position_code)]
 
 stats_dir <- "Output/NFL Stats/"
-stats_file_players <- "player_stats_2023_REG_POST_gen2024-01-11 011954.csv"
+stats_file_players <- "stats_2023_REG_POST_gen2024-01-12 014201.csv"
 stats_players <- fread(file = paste0(stats_dir,stats_file_players))
 stats_players[,player_id:=ifelse(position=="Defense",team_abbr,player_id)]
 
 stats_players <- stats_players[season_type == "Regular"]
 
-scored_roster <- merge.data.table(stats_players, roster_full, by = "player_id", all.y = TRUE, allow.cartesian=TRUE)
-
-# scored_roster <- scored_roster[stat_type == "fantasy_points"]
-# scored_roster <- scored_roster[,.(fantasy_points=sum(stat_values)), 
-#                                by = .(fantasy_owner_initials,
-#                                       fantasy_team_name,
-#                                       player_id,
-#                                       player_name,
-#                                       team_abbr)]
-# setorder(scored_roster, cols = fantasy_team_name, -fantasy_points)
-# scored_roster[,total_fantasy_points:=cumsum(fantasy_points),by=fantasy_team_name]
-
+scored_rosters <- merge.data.table(stats_players, rosters, by = "player_id", all.y = TRUE, allow.cartesian=TRUE)
 
 output_file <- paste0(
   "Output/Scored Rosters/Test NFL Fantasy Scores for 2023-2024 as of ",
@@ -84,6 +33,5 @@ output_file <- paste0(
   ".csv"
 )
 
-
-fwrite(scored_roster, output_file)
+fwrite(scored_rosters, output_file)
 
