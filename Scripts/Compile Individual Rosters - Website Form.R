28a09d3bfb585dd9a2d2e3f97eaa6892b101b2cc
hillad3 <adam.x.hill@gmail.com>
2024-01-12 03:05:01 -0500

    Created additional tests to check for errors and move data munging to better places in the process vs in the scoring scripts.

--- Scripts/Compile Individual Rosters - Website Form.R
+++ Scripts/Compile Individual Rosters - Website Form.R
@@ -5,6 +5,7 @@ gc()
 ## clear cache as needed
 # nflreadr::clear_cache()
 
+library(tidyverse)
 library(data.table)
 library(logger)
 logger::log_layout(layout_glue_colors)
@@ -60,23 +61,69 @@ lapply(rosters, check_validity)
 
 rosters <- rbindlist(rosters)
 
-rosters[,source_file := rep(roster_files, each=14)] # this should file if not perfectly rectangular
+rosters[,source_file := rep(roster_files, each=14)] # this should fail if not perfectly rectangular
 
 if (any(is.na(rosters |> unlist()))) {
   log_warn("There are NAs in the table")
   print(dt)
 }
 
-# TODO count to ensure that team names are unique and total to 14 (to avoid duplicate names from multiple people)
-# TODO take care of special characters in the Participant Info fields
-
-# drop email column before consolidating
+# drop unnecessary data. these details will be brought in from tte stats
+rosters[,`Position Group`:=NULL]
+rosters[,`Selection`:=NULL]
 rosters[,`Fantasy Owner Email`:=NULL]
+rosters[,`Check 1 - Selection is Unique`:=NULL]
+rosters[,`Check 2 - Team is Unique`:=NULL]
+
+setnames(
+  rosters,
+  old = c(
+    "Fantasy Owner",
+    "Fantasy Team Name",
+    "Automation Mapping",
+    "Roster",
+    "Position Code",
+    "Position Type",
+    "Team Abbr."
+  ),
+  new = c(
+    "fantasy_owner",
+    "fantasy_team_name",
+    "player_id",
+    "roster",
+    "position_code",
+    "position_type",
+    "team_abbr"
+  )
+)
+
+# create initials and drop names to remove PII
+rosters <- rosters[, fantasy_owner_initials:=lapply(
+  fantasy_owner,
+  function(x){
+    if(str_detect(x," ")){
+      paste0(str_sub(x,1,1),".",str_sub(x,str_locate(x," ")[,1]+1,str_locate(x," ")[,1]+1),".")
+    } else {
+      paste0(str_sub(x,1,3),".")
+    }
+  }
+)]
+rosters[,fantasy_owner_initials:=unlist(fantasy_owner_initials)]
+rosters[,fantasy_team_and_initials:=paste0(fantasy_team_name," (",fantasy_owner_initials,")")]
+rosters[,fantasy_owner:=NULL]
+rosters[,fantasy_owner_initials:=NULL]
+
+# TODO count to ensure that team names are unique and total to 14 (to avoid duplicate names from multiple people)
+tmp <- rosters[,.(fantasy_team_and_initials)][,.(n=.N), by=.(fantasy_team_and_initials)]
+
+if(any(tmp$n!=14L)){
+  stop("There is a team that does not have exactly 14 players on the roster")
+}
 
 fwrite(
   rosters,
   file = paste0(
-    "Output/Compiled Rosters/Playoff Fantasy Roster for ",
+    "Output/Compiled Rosters/Playoff Fantasy Rosters for ",
     season,
     ", Compiled ",
     str_remove_all(Sys.time(), ":"),
