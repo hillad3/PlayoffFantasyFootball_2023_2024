f90184b891806d688d2aac495d0cd6455fcf3df4
hillad3 <adam.x.hill@gmail.com>
2024-01-13 17:28:20 -0500

    Added option for trouble shooting import with fread

--- Scripts/Score Playoff Rosters - 2023-2024 Season.R
+++ Scripts/Score Playoff Rosters - 2023-2024 Season.R
@@ -11,7 +11,7 @@ library(nflreadr)
 library(openxlsx)
 
 roster_dir <- "Output/Compiled Rosters/"
-roster_file <- "Playoff Fantasy Rosters for 2023-2024, Compiled 2024-01-13 051701.csv"
+roster_file <- "Playoff Fantasy Rosters for 2023-2024, Compiled 2024-01-13 171220.csv"
 
 rosters <- fread(file = paste0(roster_dir,roster_file))
 rosters[,team_abbr:=NULL]
@@ -23,16 +23,18 @@ if("Fantasy Owner Email" %in% names(rosters)){
 }
 
 stats_dir <- "Output/NFL Stats/"
-stats_file_players <- "stats_2023_REG_POST_gen2024-01-12 014201.csv"
+stats_file_players <- "stats_2023_REG_POST_gen2024-01-13 171600.csv"
 stats_players <- fread(file = paste0(stats_dir,stats_file_players))
 stats_players[,player_id:=ifelse(position=="Defense",team_abbr,player_id)]
 
-stats_players <- stats_players[season_type == "Regular"]
+stats_players <- stats_players[season_type == "Post"]
 
 scored_rosters <- merge.data.table(stats_players, rosters, by = "player_id", all.y = TRUE, allow.cartesian=TRUE)
 
+scored_rosters <- scored_rosters[stat_type=="fantasy_points"]
+
 output_file <- paste0(
-  "Output/Scored Rosters/Test NFL Fantasy Scores for 2023-2024 as of ",
+  "Output/Scored Rosters/NFL Fantasy Scores for 2023-2024 as of ",
   str_remove_all(Sys.time(), ":"),
   ".csv"
 )
