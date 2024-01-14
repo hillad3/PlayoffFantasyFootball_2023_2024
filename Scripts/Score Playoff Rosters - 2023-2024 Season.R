82c5aba83dd6f09901c55f16f8b1a5421bafdac6
hillad3 <adam.x.hill@gmail.com>
2024-01-14 15:08:28 -0500

    Update file reference.

--- Scripts/Score Playoff Rosters - 2023-2024 Season.R
+++ Scripts/Score Playoff Rosters - 2023-2024 Season.R
@@ -23,7 +23,7 @@ if("Fantasy Owner Email" %in% names(rosters)){
 }
 
 stats_dir <- "Output/NFL Stats/"
-stats_file_players <- "stats_2023_REG_POST_gen2024-01-14 041830.csv"
+stats_file_players <- "stats_2023_REG_POST_gen2024-01-14 140540.csv"
 stats_players <- fread(file = paste0(stats_dir,stats_file_players))
 stats_players[,player_id:=ifelse(position=="Defense",team_abbr,player_id)]
 
